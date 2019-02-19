86ab1cf55f0075bfda107904936bab30e8c1b661
susan007 <786464180@qq.com>
2019-02-19 17:42:02 +0800

    '添加测试'

--- docs/.vuepress/components/hello.vue
+++ docs/.vuepress/components/hello.vue
@@ -1,27 +1,21 @@
 // 注册一个自定义组件
 <template>
-  <div class="cpt-hello">
-    <el-container>
-      <!--head-->
-      <el-header>
-        静静是小花
-      </el-header>
-      <!--content-->
-      <el-main>
-
-      </el-main>
-      <!--footer-->
-      <el-footer>
-
-      </el-footer>
-
-    </el-container>
+  <div class="cpt-hello" id="content">
+    Hello Kitty
   </div>
 </template>
 
 <script>
   export default {
-      name: 'my-hello'
+      name: 'hello',
+      mounted() {
+          this.add(1,2)
+      },
+      methods: {
+          add(a, b) {
+              return a+b
+          }
+      }
   }
 </script>
 
