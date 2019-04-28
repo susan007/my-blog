7b413c2298a7e78fa964562589426a4acd5c1ac2
susan007 <786464180@qq.com>
2019-04-28 10:44:00 +0800

    '更新博客啊'

--- docs/.vuepress/components/number-game.vue
+++ docs/.vuepress/components/number-game.vue
@@ -1,5 +1,5 @@
-<!--<template>-->
-    <!--<div class="content">-->
+<template>
+    <div class="content">
         <!--&lt;!&ndash;tip start&ndash;&gt;-->
         <!--<el-alert-->
                 <!--:title="msgTitle"-->
@@ -20,73 +20,73 @@
             <!--</el-form-item>-->
         <!--</el-form>-->
         <!--&lt;!&ndash;form end&ndash;&gt;-->
-    <!--</div>-->
-<!--</template>-->
+    </div>
+</template>
 
-<!--<script>-->
-    <!--export default {-->
-        <!--name: 'number-game',-->
-        <!--data() {-->
-            <!--return {-->
-                <!--ruleForm: {-->
-                    <!--answer: ''-->
-                <!--},-->
-                <!--rules: {-->
-                    <!--answer: { required: true, min: 1, max: 2, message: '输入1~99之间的数字', trigger: 'blur' }-->
-                <!--},-->
-                <!--question: null,-->
-                <!--msgTitle: '输入猜想，点击提交开始游戏！',-->
-                <!--msgType: 'warning'-->
-            <!--}-->
-        <!--},-->
-        <!--mounted() {-->
-            <!--// this.handleNumber()-->
-        <!--},-->
-        <!--methods: {-->
-            <!--handleNumber() {-->
-                <!--// 随机生成1~99的数字并取整-->
-                <!--this.question = Math.floor(Math.random() * 100)-->
-            <!--},-->
-            <!--submitForm(formName) {-->
-                <!--this.$refs[formName].validate((valid) => {-->
-                    <!--if (valid) {-->
-                        <!--const answer = parseInt(this.ruleForm.answer)-->
-                        <!--if (answer > this.question) {-->
-                            <!--this.msgTitle = `你的答案是${answer}，猜大了！`-->
-                            <!--this.msgType = 'error'-->
-                        <!--} else if (answer < this.question) {-->
-                            <!--this.msgTitle = `你的答案是${answer}，猜小了！`-->
-                            <!--this.msgType = 'error'-->
-                        <!--} else if (answer === this.question) {-->
-                            <!--this.msgTitle = '你猜对了！再来一局吧'-->
-                            <!--this.msgType = 'success'-->
-                            <!--this.handleNumber()-->
-                        <!--} else {-->
-                            <!--this.msgTitle = '你猜的是什么玩意？'-->
-                            <!--this.msgType = 'error'-->
-                        <!--}-->
-                    <!--} else {-->
-                        <!--return false-->
-                    <!--}-->
-                <!--})-->
-            <!--}-->
-        <!--}-->
-    <!--}-->
-<!--</script>-->
+<script>
+    export default {
+        name: 'number-game',
+        data() {
+            return {
+                // ruleForm: {
+                //     answer: ''
+                // },
+                // rules: {
+                //     answer: { required: true, min: 1, max: 2, message: '输入1~99之间的数字', trigger: 'blur' }
+                // },
+                // question: null,
+                // msgTitle: '输入猜想，点击提交开始游戏！',
+                // msgType: 'warning'
+            }
+        },
+        mounted() {
+            // this.handleNumber()
+        },
+        methods: {
+            handleNumber() {
+                // 随机生成1~99的数字并取整
+                this.question = Math.floor(Math.random() * 100)
+            },
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        const answer = parseInt(this.ruleForm.answer)
+                        if (answer > this.question) {
+                            this.msgTitle = `你的答案是${answer}，猜大了！`
+                            this.msgType = 'error'
+                        } else if (answer < this.question) {
+                            this.msgTitle = `你的答案是${answer}，猜小了！`
+                            this.msgType = 'error'
+                        } else if (answer === this.question) {
+                            this.msgTitle = '你猜对了！再来一局吧'
+                            this.msgType = 'success'
+                            this.handleNumber()
+                        } else {
+                            this.msgTitle = '你猜的是什么玩意？'
+                            this.msgType = 'error'
+                        }
+                    } else {
+                        return false
+                    }
+                })
+            }
+        }
+    }
+</script>
 
-<!--<style scoped>-->
-    <!--.content{-->
-        <!--width: 100%;-->
-        <!--height: 100%;-->
-        <!--text-align: center;-->
-        <!--background-image: url("../public/bg_sweet.png");-->
-    <!--}-->
+<style scoped>
+    .content{
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        background-image: url("../public/bg_sweet.png");
+    }
 
-    <!--.tip{-->
-        <!--padding: 20px 30px;-->
-        <!--margin-bottom: 50px;-->
-        <!--width: 50%;-->
-        <!--margin-left: 25%;-->
-        <!--margin-right: 50%;-->
-    <!--}-->
-<!--</style>-->
+    .tip{
+        padding: 20px 30px;
+        margin-bottom: 50px;
+        width: 50%;
+        margin-left: 25%;
+        margin-right: 50%;
+    }
+</style>
