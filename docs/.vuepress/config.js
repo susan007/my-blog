10e52eaf08fb7c139e0733e9492e884bf7bfd1ee
susan007 <786464180@qq.com>
2019-04-28 11:19:09 +0800

    '添加博客'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -163,13 +163,11 @@ module.exports = {
                     collapsable: true,
                     children: [
                         ['/PERSONAL/search', '纯粹搜索'],
-                        ['/PERSONAL/boolgame', '恶魔圈'],
-                        ['/PERSONAL/猜数字', '猜数字'] // travis-ci无法build，很尴尬！
+                        ['/PERSONAL/boolgame', '恶魔圈']
+                        // ['/PERSONAL/猜数字', '猜数字'] // travis-ci无法build，很尴尬！
                     ]
                 }
             ]
         }
     }
-
 }
-
