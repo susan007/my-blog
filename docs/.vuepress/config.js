477f27363abf52ef4b2b438c6888ac5c6f2f2ac3
susan007 <786464180@qq.com>
2019-06-11 11:00:40 +0800

    '添加博客函数'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,7 +57,8 @@ module.exports = {
                         ['/WEB/JS/封闭执行', 'setTimeout()和for循环'],
                         ['/WEB/JS/外部js文件执行顺序', '脚本调用策略'],
                         ['/WEB/JS/object', '创建对象的几种方式'],
-                        ['/WEB/JS/dom', 'js操作Dom常用方法']
+                        ['/WEB/JS/dom', 'js操作Dom常用方法'],
+                        ['/WEB/JS/function', '函数']
                     ]
                 }, {
                     title: 'CSS',
